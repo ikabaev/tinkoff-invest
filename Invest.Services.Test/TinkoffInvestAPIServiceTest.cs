0c496e299d1db562528d3c3f2fc8b2848b942df3
Igor Kabaev <ikabaev@gmail.com>
2024-11-14 20:57:37 +0300

    + diagram

--- Invest.Services.Test/TinkoffInvestAPIServiceTest.cs
+++ Invest.Services.Test/TinkoffInvestAPIServiceTest.cs
@@ -2,9 +2,21 @@ namespace Invest.Services.Test
 {
     public class TinkoffInvestAPIServiceTest
     {
+        List<float> _quotes = [];
+
         [SetUp]
         public void Setup()
         {
+        }
+
+        private bool TestOpen(float quote)
+        {
+            return false;
+        }
+
+        private bool TestClose(float quote)
+        {
+            return false;
         }
 
         [Test]
@@ -13,6 +25,22 @@ namespace Invest.Services.Test
             //var service = new TinkoffInvestAPIService();
             //var result = service.OpenSandboxAccount();
             //Assert.True(result);
+        }
+
+        [Test]
+        public void MaxPL()
+        {
+            foreach (var quote in _quotes)
+            {
+                if (this.TestOpen(quote)) { 
+                    
+                }
+
+                if (this.TestClose(quote))
+                {
+
+                }
+            }
         }
     }
 }
