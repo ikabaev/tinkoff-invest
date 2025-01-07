32f983ee4b240e15e9135152612f537b0ceafb1a
Igor Kabaev <ikabaev@gmail.com>
2025-01-07 12:29:36 +0300

    load moex.sber_1

--- Invest.Services.Quotes/Program.cs
+++ Invest.Services.Quotes/Program.cs
@@ -1,7 +1,51 @@
-using Invest.Services.Quotes;
+//using Invest.Services.Quotes;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+//var builder = Host.CreateApplicationBuilder(args);
+//builder.Services.AddHostedService<Worker>();
 
-var host = builder.Build();
-host.Run();
+//var host = builder.Build();
+//host.Run();
+
+
+using Dapper;
+using HelgiLab.Extentions.Linq;
+using Npgsql;
+using System.Globalization;
+
+var source = "C:\\BackUp\\Quotes\\MOEX_SBER\\MOEX_SBER_1_20200101_20250105.csv";
+var connectionString = "Host=frog;Database=quotes;Username=postgres;Password=postgre;";
+var BATCH_SIZE = 100000;
+
+IFormatProvider provider = CultureInfo.InvariantCulture;
+
+var quoteBatches = File.ReadLines(source)
+    .Select(x => x.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+    .SkipWhile(x => x[0].Equals("<TICKER>", StringComparison.OrdinalIgnoreCase))
+    .Select(x => new { 
+        Ticker = x[0], 
+        Period = int.Parse(x[1]),
+        Dttm = DateTimeOffset.ParseExact(x[2] + x[3], "yyyyMMddHHmmss", provider).ToUniversalTime(),
+        Open = float.Parse(x[4], provider),
+        High = float.Parse(x[5], provider),
+        Low = float.Parse(x[6], provider),
+        Close = float.Parse(x[7], provider),
+        Volume = int.Parse(x[8], provider),
+    })
+    .SplitIf(list => list.Count == BATCH_SIZE)
+    ;
+
+const string sql = @"INSERT INTO
+    moex.sber_1 (dttm, open, high, low, close, volume) 
+    VALUES (@Dttm, @Open, @High, @Low, @Close, @Volume)";
+
+var rowsAffected = 0;
+foreach (var batch in quoteBatches)
+{
+    using var connection = new NpgsqlConnection(connectionString);
+    connection.Open();
+
+    using var transaction = connection.BeginTransaction();
+    rowsAffected += connection.Execute(sql, batch, transaction: transaction);
+    transaction.Commit();
+    Console.WriteLine($"{rowsAffected} row(s) affected.");
+}
---
+++ Ivka.Invest.TradeService/EncoderDecoderModel.cs
@@ -0,0 +1,19 @@
+﻿using TorchSharp.Modules;
+using static TorchSharp.torch;
+
+namespace Ivka.Invest.Trade
+{
+    internal class EncoderDecoderModel : nn.Module
+    {
+        private Embedding _encoder_embed;
+
+        protected EncoderDecoderModel(string name) : base(name)
+        {
+            _encoder_embed = nn.Embedding(1, 2, 3);
+        }
+        //protected EncoderDecoderModel(string name) : base(name)
+        //{
+        //    this.encoder_embed = nn.Embedding(input_voc_size, self.input_size)
+        //}
+    }
+}
